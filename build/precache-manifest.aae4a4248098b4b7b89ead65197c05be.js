87b04b7ea0eb31015a130e4d513920409cc38b5a
Jacob Metcalfe <jmetcalf@uccs.edu>
2021-01-05 15:43:47 -0700

    Changed Web Description

---
+++ build/precache-manifest.aae4a4248098b4b7b89ead65197c05be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cba5a4a24f8f29cd5a9f7879d9396d92",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3f22de0102867952f9f5",
+    "url": "/static/css/main.35031b02.chunk.css"
+  },
+  {
+    "revision": "d22328d5fcf1f0506e09",
+    "url": "/static/js/2.56a4b2a0.chunk.js"
+  },
+  {
+    "revision": "b01cf20ee08477db845d02c3b279da13",
+    "url": "/static/js/2.56a4b2a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f22de0102867952f9f5",
+    "url": "/static/js/main.4760fc8e.chunk.js"
+  },
+  {
+    "revision": "2b5439e16cf819a1e33c",
+    "url": "/static/js/runtime-main.a1041db0.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.251ab7e2e0cb400774209ffb6c7307de.js"
+  "/precache-manifest.aae4a4248098b4b7b89ead65197c05be.js"
 );
 
 self.addEventListener('message', (event) => {
